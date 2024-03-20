aa71b0be209d63ccc966d8c0c5501a0e8e06eb12
Gabriel Rey <rgabi1219@gmail.com>
2024-03-20 15:27:44 -0300

    Actualizacion de datos en pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <>
       <h1>Rey Gabriel Manuel Alejandro</h1>
-      <h4>Desarrollador FullStack JS Trainee</h4>
+      <h2>Desarrollador FullStack JS Trainee</h2>
+      <h4>Cursos realizados</h4>
+      <ul><li>Infomatorio Etapa 1 y 2 - 2023</li><li>SummerHack CodeaRock - 2024</li></ul>
     </>
   )
 }

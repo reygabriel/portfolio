64ff47e7d7e9cced03fa2a72aa5b56554e002440
Gabriel Rey <rgabi1219@gmail.com>
2024-03-20 15:14:30 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Rey Gabriel Manuel Alejandro</h1>
+      <h4>Desarrollador FullStack JS Trainee</h4>
+    </>
+  )
+}
+
+export default App
